9ceca140563869090a1b2ecc36b546605579a1b5
dansuissa <150948900+dansuissa@users.noreply.github.com>
2025-05-01 16:28:36 +0200

    Update and rename Vector.h to ray-tracer/Vector.h

--- ray-tracer/Vector.h
+++ ray-tracer/Vector.h
@@ -6,16 +6,14 @@ struct Vec3{
     double x{}, y{}, z{};
     constexpr Vec3() = default;
     constexpr Vec3(double X,double Y,double Z):x(X),y(Y),z(Z){}
-    Vec3  operator+(const Vec3& o) const {return {x+o.x, y + o.y, z+o.z};}
-    Vec3  operator-(const Vec3& o) const {return {x- o.x, y-o.y, z-o.z};}
-    Vec3  operator-() const {return {-x,-y,-z};}
-    Vec3  operator*(double s) const {return {x*s, y* s, z*s};}
-    Vec3  operator/(double s) const {return {x /s, y/s, z/s};}
+    Vec3 operator+(const Vec3& o) const {return {x+o.x, y + o.y, z+o.z};}
+    Vec3 operator-(const Vec3& o) const {return {x- o.x, y-o.y, z-o.z};}
+    Vec3 operator-() const {return {-x,-y,-z};}
+    Vec3 operator*(double s) const {return {x*s, y* s, z*s};}
+    Vec3 operator/(double s) const {return {x /s, y/s, z/s};}
     Vec3& operator+=(const Vec3& o){ x+=o.x; y+=o.y; z+=o.z; return *this;}
-
-    /* geometry ----------------------------------------------------------- */
     double norm2() const {return x*x + y*y + z*z;}
-    double norm() const { return std::sqrt(norm2());}
+    double norm() const {return std::sqrt(norm2());}
     void normalize(){double n = norm(); x/=n; y/=n; z/=n;}
 };
 inline Vec3 operator*(double s,const Vec3& v){return v*s;}
