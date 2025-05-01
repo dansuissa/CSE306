2baa7f603eca420338231b6e4b1e15500be05660
dansuissa <150948900+dansuissa@users.noreply.github.com>
2025-05-01 16:27:16 +0200

    Update Geometry.h

--- ray-tracer/Geometry.h
+++ ray-tracer/Geometry.h
@@ -2,13 +2,13 @@
 #include "Vector.h"
 #include "Ray.h"
 
-namespace rt {
-struct Hit { double t; Vec3 P,N; };
-class Geometry {
+namespace rt{
+struct Hit {double t; Vec3 P,N;};
+class Geometry{
 public:
-    Vec3  albedo {1,1,1};
-    bool  isMirror= false;
-    bool  isTransparent = false;
+    Vec3 albedo {1,1,1};
+    bool isMirror= false;
+    bool isTransparent = false;
     virtual bool intersect(const Ray&,Hit&) const = 0;
     virtual ~Geometry() =default;
 };
