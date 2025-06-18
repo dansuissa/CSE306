31802595e69786e34bb9fe928139864254759cbf
dansuissa <150948900+dansuissa@users.noreply.github.com>
2025-06-18 14:40:05 +0200

    Update Voronoi.h

--- fluid-simulation/Voronoi.h
+++ fluid-simulation/Voronoi.h
@@ -7,21 +7,16 @@
 struct PointCloudAdapter
 {
     const std::vector<Vec2>& m_points;
-
     PointCloudAdapter(const std::vector<Vec2>& points) : m_points(points) {}
-
     inline size_t kdtree_get_point_count() const { return m_points.size(); }
-
     inline double kdtree_get_pt(const size_t idx, const size_t dim) const
     {
         if (dim == 0) return m_points[idx].x;
         else return m_points[idx].y;
     }
-
     template <class BBOX>
     bool kdtree_get_bbox(BBOX& /* bb */) const { return false; }
 };
-
 class Voronoi
 {
 public:
