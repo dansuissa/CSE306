81ff534471348a90b29560ba843686e5defebbc6
dansuissa <150948900+dansuissa@users.noreply.github.com>
2025-06-18 14:35:51 +0200

    Update PowerDiagram.h

--- fluid-simulation/PowerDiagram.h
+++ fluid-simulation/PowerDiagram.h
@@ -8,12 +8,9 @@ class PowerDiagram
 public:
     PowerDiagram(const std::vector<Vec2>& sites,
                  const std::vector<double>& weights);
-
     void   compute();                
     double area(std::size_t i) const; 
-
     std::vector<Polygon> cells;      
-
 private:
     std::vector<Vec2>    p;          
     std::vector<double>  w;          
