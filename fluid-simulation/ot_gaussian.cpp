cb1fae71e6cd63851ae94d3b2d78f30643a525fb
dansuissa <150948900+dansuissa@users.noreply.github.com>
2025-06-18 14:40:25 +0200

    Update ot_gaussian.cpp

--- fluid-simulation/ot_gaussian.cpp
+++ fluid-simulation/ot_gaussian.cpp
@@ -4,14 +4,11 @@
 int main()
 {
     Polygon box; box.v = { {0,0},{1,0},{1,1},{0,1} };
-
     const int N = 3000;
     std::vector<Vec2> sites(N);
     std::default_random_engine eng(1);
     std::uniform_real_distribution<double> U(0.,1.);
-
     for (auto& s : sites) { s.x = U(eng); s.y = U(eng); }
-
     OT solver(box, sites, 400);
     solver.run();
     solver.saveSVG("power_uniform.svg");
